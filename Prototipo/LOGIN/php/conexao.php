2580453148425b0b10a5f4a99d8a472314c5c0ac
Etec Alice <leopierini06@gmail.com>
2024-06-18 20:33:41 -0300

    conexao com o banco de dados

--- Prototipo/LOGIN/php/conexao.php
+++ Prototipo/LOGIN/php/conexao.php
@@ -11,6 +11,4 @@
     if ($conn->connect_error) {
     die("Erro na conexão: " . $conn->connect_error);
     }
-
-    echo "Conexão bem-sucedida"; // Exibir mensagem se a conexão for bem-sucedida
 ?>
